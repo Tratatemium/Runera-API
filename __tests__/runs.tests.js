5a44fd6d5d4904938ee4a882fdc4184d7ab40af2
Vladislav Zhuravskii <visitanoinvitado@gmail.com>
2026-01-20 17:55:57 +0100

    Feature/add tests for POST /runs/new-run endpoint

--- __tests__/runs.tests.js
+++ __tests__/runs.tests.js
@@ -11,7 +11,7 @@ describe("GET /runs/:id", () => {
     const runID = "dc9822e7-72d6-4cc8-b6da-c1c5208d6109";
 
     const res = await request(app).get(`/runs/${runID}`);
-    
+
     expect(res.statusCode).toBe(200);
     expect(res.headers["content-type"]).toMatch(/json/);
 
@@ -48,6 +48,114 @@ describe("GET /runs/:id", () => {
     expect(res.headers["content-type"]).toMatch(/json/);
 
     expect(res.body).toBeDefined();
+    expect(res.body).toHaveProperty("error");
+  });
+});
+
+
+describe("POST /runs/new-run", () => {
+  it("returns 201 and creates a new run with valid data", async () => {
+    const newRun = {
+      userId: "1d9a8400-07cd-466a-9d13-843a544a5b09",
+      startTime: "2026-01-19T12:25:44.822Z",
+      durationSec: 457,
+      distanceMeters: 1574
+    };
+
+    const res = await request(app).post("/runs/new-run").send(newRun);
+
+    expect(res.statusCode).toBe(201);
+    expect(res.headers["content-type"]).toMatch(/json/);
+    expect(res.body).toHaveProperty("id");
+  });
+
+  it("returns 415 for wrong Content-Type", async () => {
+    const res = await request(app).post("/runs/new-run");
+
+    expect(res.statusCode).toBe(415);
+    expect(res.headers["content-type"]).toMatch(/json/);
+    expect(res.body).toHaveProperty("error");
+  });
+
+  it("returns 400 for empty JSON", async () => {
+    const res = await request(app).post("/runs/new-run").send({});
+
+    expect(res.statusCode).toBe(400);
+    expect(res.headers["content-type"]).toMatch(/json/);
+    expect(res.body).toHaveProperty("error");
+  });
+
+  it("returns 400 for missing duration field", async () => {
+    const invalidRun = {
+      userId: "1d9a8400-07cd-466a-9d13-843a544a5b09",
+      startTime: "2026-01-11T14:45:44.822Z",
+      distanceMeters: 1727
+    };
+
+    const res = await request(app).post("/runs/new-run").send(invalidRun);
+
+    expect(res.statusCode).toBe(400);
+    expect(res.headers["content-type"]).toMatch(/json/);
+    expect(res.body).toHaveProperty("error");
+  });
+
+  it("returns 400 for invalid userId format", async () => {
+    const invalidRun = {
+      userId: "fagfgsg5054a6wa6ew1",
+      startTime: "2026-01-11T14:45:44.822Z",
+      durationSec: 1550,
+      distanceMeters: 1727
+    };
+
+    const res = await request(app).post("/runs/new-run").send(invalidRun);
+
+    expect(res.statusCode).toBe(400);
+    expect(res.headers["content-type"]).toMatch(/json/);
+    expect(res.body).toHaveProperty("error");
+  });
+
+  it("returns 400 for invalid date format", async () => {
+    const invalidRun = {
+      userId: "1d9a8400-07cd-466a-9d13-843a544a5b09",
+      startTime: "2026-31-11T14:45:44.822Z",
+      durationSec: 1550,
+      distanceMeters: 1727
+    };
+
+    const res = await request(app).post("/runs/new-run").send(invalidRun);
+
+    expect(res.statusCode).toBe(400);
+    expect(res.headers["content-type"]).toMatch(/json/);
+    expect(res.body).toHaveProperty("error");
+  });
+
+  it("returns 400 for invalid duration value", async () => {
+    const invalidRun = {
+      userId: "1d9a8400-07cd-466a-9d13-843a544a5b09",
+      startTime: "2026-01-11T14:45:44.822Z",
+      durationSec: "-1455a",
+      distanceMeters: 1727
+    };
+
+    const res = await request(app).post("/runs/new-run").send(invalidRun);
+
+    expect(res.statusCode).toBe(400);
+    expect(res.headers["content-type"]).toMatch(/json/);
+    expect(res.body).toHaveProperty("error");
+  });
+
+  it("returns 400 for invalid distance value", async () => {
+    const invalidRun = {
+      userId: "1d9a8400-07cd-466a-9d13-843a544a5b09",
+      startTime: "2026-01-11T14:45:44.822Z",
+      durationSec: 1727,
+      distanceMeters: "-1455a"
+    };
+
+    const res = await request(app).post("/runs/new-run").send(invalidRun);
+
+    expect(res.statusCode).toBe(400);
+    expect(res.headers["content-type"]).toMatch(/json/);
     expect(res.body).toHaveProperty("error");
   });
 });
