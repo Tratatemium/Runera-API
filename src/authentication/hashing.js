76c0b18c1d3592ba274ccae69e36f0e4429a867d
Vladislav Zhuravskii <visitanoinvitado@gmail.com>
2026-01-23 11:37:46 +0100

    Feature/add login functionality and update test cases

--- src/authentication/hashing.js
+++ src/authentication/hashing.js
@@ -1,7 +1,10 @@
 const bcrypt = require("bcrypt");
+const db = require("../database.js");
 
 const saltRounds = 10;
 const algorithm = "bcrypt";
+const DUMMY_HASH =
+  "$2b$10$CwTycUXWue0Thq9StjUM0uJ8pJ6D0sJ1uQe8kJZ9nY9E6kKq6Zq9W";
 
 const createPasswordHash = async (plainTextPassword) => {
   const passwordHash = await bcrypt.hash(plainTextPassword, saltRounds);
@@ -12,6 +15,18 @@ const createPasswordHash = async (plainTextPassword) => {
     lockUntil: null,
   };
   return { passwordHash, passwordMetadata };
+};
+
+const login = async (email, password) => {
+  const foundUser = db.findUserByField("email", email);
+
+  const passwordHash = foundUser ? foundUser.passwordHash : DUMMY_HASH;
+  const isPasswordCorrect = await bcrypt.compare(password, passwordHash);
+
+  if (foundUser && isPasswordCorrect) {
+    
+  }
+
 };
 
 module.exports = {
