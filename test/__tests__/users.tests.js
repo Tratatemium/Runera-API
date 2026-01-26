63f20381e570519281dc04fac4ffdb1bc7552234
Vladislav Zhuravskii <visitanoinvitado@gmail.com>
2026-01-26 21:50:16 +0100

    Refactor: remove commented-out user data examples from users tests

--- test/__tests__/users.tests.js
+++ test/__tests__/users.tests.js
@@ -1,17 +1,6 @@
 const request = require("supertest");
 const app = require("../../src/app.js");
 
-  // {
-  // "username": "test_runner_01",
-  // "password": "TestPassword123!",
-  // "email": "runner01@test.com"
-  // },
-  // {
-  //   "username": "test_runner_02",
-  //   "password": "SecurePass456!",
-  //   "email": "runner02@test.com"
-  // }
-
 describe("POST /users/ - Integration Tests", () => {
   const validUserData = {
     username: "testuser123",
