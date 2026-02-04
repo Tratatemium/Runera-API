b98932a335ac1c96ab112d2fd61026c9a30c12c2
Vladislav Zhuravskii <visitanoinvitado@gmail.com>
2026-02-04 12:12:08 +0100

    Feature: implement get user by ID route with validation and permissions

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -4,10 +4,19 @@ const router = express.Router();
 const usersValidation = require("../middleware/validation/users.validation.js");
 const runsValidation = require("../middleware/validation/runs.validation.js");
 const authMiddleware = require("../middleware/auth.middleware.js");
+const guardMiddleware = require("../middleware/guard.middleware.js");
 const usersController = require("../controllers/users.controller.js");
 const runsController = require("../controllers/runs.controller.js");
 
 // NOTE: possibly add guard middleware to check if user is active / banned / etc.
+
+router.get(
+  ":/id",
+  usersValidation.validateUUID("id"),
+  authMiddleware.checkAuth,
+  guardMiddleware.checkPermissions({ param: "id", type: "userId" }),
+  usersController.getUserById,
+);
 
 /* ================================================================================================= */
 /*  User (me)                                                                                        */
