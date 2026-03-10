2dc66fddb43adffd54c2a4150115f18794c0bf16
Vladislav Zhuravskii <visitanoinvitado@gmail.com>
2026-03-10 16:35:34 +0100

    Refactor database connection handling to use promise chaining

--- api/index.js
+++ api/index.js
@@ -3,14 +3,20 @@ const app = require("../src/app.js");
 const { connectDB } = require("../src/utils/db.utils.js");
 
 const handler = serverless(app);
-
 let connected = false;
 
-module.exports = async (req, res) => {
+module.exports = (req, res) => {
   if (!connected) {
-    await connectDB();
-    connected = true;
+    connectDB()
+      .then(() => {
+        connected = true;
+        handler(req, res);
+      })
+      .catch(err => {
+        console.error("DB connection error:", err);
+        res.status(500).json({ error: "DB connection failed" });
+      });
+  } else {
+    handler(req, res);
   }
-
-  return handler(req, res);
 };
