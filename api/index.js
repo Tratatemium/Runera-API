e1948aa19336082d89a6741f64c67b601cc6d1f2
Vladislav Zhuravskii <visitanoinvitado@gmail.com>
2026-03-11 17:43:00 +0100

    Refactor database connection logic to use an immediately invoked async function expression

--- api/index.js
+++ api/index.js
@@ -3,13 +3,15 @@ const { connectDB } = require("../src/utils/db.utils.js");
 
 let connected = false;
 
-try {
-  if (!connected) {
-    await connectDB();
-    connected = true;
+(async () => {
+  try {
+    if (!connected) {
+      await connectDB();
+      connected = true;
+    }
+  } catch (err) {
+    console.error("DB connection failed:", err);
   }
-} catch (err) {
-  console.error("DB connection failed:", err);
-}
+})();
 
-export default app;
+module.exports = app;
