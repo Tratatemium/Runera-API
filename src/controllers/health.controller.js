3ed7320ce58151e7e9d30e762e3b18a2ff7429f2
Vladislav Zhuravskii <visitanoinvitado@gmail.com>
2026-03-11 21:12:20 +0100

    Remove sendSuccess utility and directly respond with JSON in health controller

--- src/controllers/health.controller.js
+++ src/controllers/health.controller.js
@@ -1,9 +1,8 @@
-const { sendSuccess } = require("../utils/response.utils.js");
 const { checkDBconnection } = require("../utils/db.utils.js");
 const { getUptime } = require("../utils/server.utils.js");
 
 const healthController = (req, res) => {
-  sendSuccess(res, 200, {
+  res.status(200).json({
     status: "running",
     uptime: getUptime(),
     version: "1.0.0",
