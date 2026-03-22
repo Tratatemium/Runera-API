8fa3145a520ae069cdc5395971e73ce1f1d3ff9c
Vladislav Zhuravskii <visitanoinvitado@gmail.com>
2026-03-22 15:42:45 +0100

    Add cookie-parser middleware to app.js

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@
 
 const express = require("express");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 const app = express();
 
 /* ================================================================================================= */
@@ -11,6 +12,8 @@ const app = express();
 /* ================================================================================================= */
 
 app.use(express.json());
+
+app.use(cookieParser());
 
 app.use(
   cors({
