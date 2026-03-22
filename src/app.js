4d4d24a32ee8870219efad6bb45a126dab48ef6b
Vladislav Zhuravskii <visitanoinvitado@gmail.com>
2026-03-22 16:14:43 +0100

    Update CORS origin to use HTTPS for cookie security

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://localhost:3000",
     methods: ["GET", "POST", "OPTIONS"],
     credentials: true,
   }),
